9f9f778e199d4d238764b88161b80db42fc04328
Damian <damian@d2600.com>
2026-09-13 22:38:33 -0700

    cambiando link del debug en gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
             // se alcanza por 10.0.2.2. Con un teléfono físico por USB, usa
             // la IP de tu computadora en la red local.
 //            buildConfigField("String", "API_BASE", "\"http://10.0.2.2:8080\"")
-            buildConfigField("String", "API_BASE", "\"192.168.110.89:8080\"")
+            buildConfigField("String", "API_BASE", "\"http://192.168.110.89:8080\"")
         }
     }
 
