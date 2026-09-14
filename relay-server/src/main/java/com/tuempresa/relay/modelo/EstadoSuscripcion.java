dae2ac0a7c88fb89d8eba60ea539cf371d7aa2be
Damian <damian@d2600.com>
2026-09-13 18:18:15 -0700

    cambios relaizados para que compile por CL

--- relay-server/src/main/java/com/tuempresa/relay/modelo/EstadoSuscripcion.java
+++
@@ -1,9 +0,0 @@
-package com.tuempresa.relay.modelo;
-
-/** Estado de una suscripción WebSub. El panel lo usa para pintar el testigo. */
-public enum EstadoSuscripcion {
-    PENDIENTE_VERIFICACION,
-    ACTIVA,
-    CANCELADA,
-    ERROR
-}
